e1456409be87471d6fe9ec9f4082db1c708d325c
kyleblocksom <73256380+kyleblocksom@users.noreply.github.com>
2023-10-09 11:53:41 -0700

    Update fsi_agent.py and tools.py with latest Claude Bedrock prompt input format

--- agent/lambda/agent-handler/tools.py
+++ agent/lambda/agent-handler/tools.py
@@ -39,12 +39,13 @@ class Tools():
         )
 
         prompt_template = """
-        The following is a friendly conversation between a human and an AI. 
+        \n\nHuman: The following is a friendly conversation between a human and an AI. 
         The AI is talkative and provides lots of specific details from its context.
         If the AI does not know the answer to a question, it truthfully says it 
         does not know.
         {context}
-        Instruction: Based on the above documents, provide a detailed answer and source document for, {question} Answer "don't know" if not present in the document. Solution:
+        Instruction: Based on the above documents, provide a detailed answer and source document for, {question} Answer "don't know" if not present in the document.
+        \n\nAssistant:
         """
 
         PROMPT = PromptTemplate(
